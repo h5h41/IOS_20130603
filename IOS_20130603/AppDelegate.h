0893e7b80ce22a5e5e05c0b7771e1cd171208d0e
张翼德 <zhangjian@bogon.(none)>
2013-06-03 13:34:10 +0800

    Initial Commit

---
+++ IOS_20130603/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOS_20130603
+//
+//  Created by 张翼德 on 13-6-3.
+//  Copyright (c) 2013年 张飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOS_20130603Tests/IOS_20130603Tests.h
@@ -0,0 +1,13 @@
+//
+//  IOS_20130603Tests.h
+//  IOS_20130603Tests
+//
+//  Created by 张翼德 on 13-6-3.
+//  Copyright (c) 2013年 张飞. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IOS_20130603Tests : SenTestCase
+
+@end
